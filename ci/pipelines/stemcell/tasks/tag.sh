63cd17b9a13d6fc7dee83bd53c142a23a00a323c
Zachary Auerbach <zauerbach@pivotal.io>
2016-09-27 15:55:19 -0700

    alsdfkj
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- ci/pipelines/stemcell/tasks/tag.sh
+++ ci/pipelines/stemcell/tasks/tag.sh
@@ -4,7 +4,12 @@ set -e
 set -u
 
 export VERSION=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
-export GIT_SSL_NO_VERIFY=true
+
+mkdir -p ~/.ssh
+cat > ~/.ssh/config <<EOF
+StrictHostKeyChecking no
+LogLevel quiet
+EOF
 
 cd bosh-src
 
