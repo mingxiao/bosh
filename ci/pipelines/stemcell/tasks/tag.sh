10eaa80b22267b3eee71f4e1e68757e66a9ac565
Ming Xiao <mxiao@pivotal.io>
2016-09-27 16:00:59 -0700

    what
    Signed-off-by: Zachary Auerbach <zauerbach@pivotal.io>

--- ci/pipelines/stemcell/tasks/tag.sh
+++ ci/pipelines/stemcell/tasks/tag.sh
@@ -5,12 +5,6 @@ set -u
 
 export VERSION=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
 
-mkdir -p ~/.ssh
-cat > ~/.ssh/config <<EOF
-StrictHostKeyChecking no
-LogLevel quiet
-EOF
-
 cd bosh-src
 
 git tag stable-$VERSION
